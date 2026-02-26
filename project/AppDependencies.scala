c736a51d5e087722b5162bfde7816206d31aa1ae
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2026-02-26 16:43:37 +0000

    VEI-789 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
